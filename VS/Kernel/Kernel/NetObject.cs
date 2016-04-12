c73cb449037851e6b270251d6f95ca5be8f46991
TankleL <Tankle_@hotmail.com>
2016-04-12 22:08:50 +0800

    added new class: NetObject

---
+++ VS/Kernel/Kernel/NetObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    abstract class NetObject
+    {
+        #region Constructors
+        NetObject();
+        
+        #endregion
+        
+
+        #region Interfaces
+        public virtual void Send();
+        public virtual void Receive();
+        #endregion
+    }
+}
