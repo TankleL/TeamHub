21fd0e7c6e8d7dc5b71fe37387316a4d4c0f9d0b
TankleL <Tankle_@hotmail.com>
2016-04-12 22:29:28 +0800

    added new class: NetServer

---
+++ VS/Kernel/Kernel/NetServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    public delegate int NetProcedure();
+    abstract class NetServer : NetObject
+    {
+        #region Constructors
+        NetServer();
+        #endregion
+
+        #region Interfaces
+        public virtual void Listen();
+        public virtual void Accept();
+        public virtual void SetProcedure(NetProcedure procedure);
+        #endregion
+
+        #region Properties
+        protected NetProcedure _procedure;
+        #endregion
+    }
+}
